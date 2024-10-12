48fb85c5f582e4c1510b8851e27bdf6d4f94c026
Bazouz660 <basile.trebus-hamann@epitech.eu>
2024-10-12 21:33:32 +0200

    [ADD]: advanced raycast character controller

--- scene/player/player.gd
+++ scene/player/player.gd
@@ -2,26 +2,36 @@ extends CharacterBody3D
 
 class_name Player
 
+@onready var camera_3d = $CameraPivot/Camera3D
+@onready var step_cast = $StepCast
+@onready var camera_pivot = $CameraPivot
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-@onready var camera_3d = $CameraPivot/Camera3D
+const STEP_INTERPOLATION_SPEED = 20.0
+
+var yaw = 0.0;
+var pitch = 0.0;
+var sensitivity = 0.003;
+
+var grounded = false;
 
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
 	
 func _ready():
 	camera_3d.current = is_multiplayer_authority()
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED;
 
 func handle_input(_delta: float):
-	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_grounded():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, yaw)
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -31,10 +41,41 @@ func handle_input(_delta: float):
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_grounded():
 		velocity += get_gravity() * delta
 		
 	if is_multiplayer_authority():
 		handle_input(delta)
 
+	move(delta);
+	
+func move(delta: float):
+	var query = PhysicsShapeQueryParameters3D.new()
+	query.exclude = [self]
+	query.shape = step_cast.shape
+	query.transform = step_cast.global_transform
+	var result = get_world_3d().direct_space_state.intersect_shape(query, 1)
+	if !result:
+		step_cast.force_shapecast_update()
+	
+	if step_cast.is_colliding() && absf(velocity.y) <= 0.0001 && !result:
+		var collision_point = step_cast.get_collision_point(0)
+		var step_height = collision_point.y - global_position.y
+		global_position.y = lerp(global_position.y, collision_point.y, delta * STEP_INTERPOLATION_SPEED)
+		velocity.y = 0.0
+		grounded = true
+	else:
+		grounded = false
+		
 	move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		yaw -= event.relative.x * sensitivity
+		#pitch -= event.relative.y * sensitivity
+		#pitch = clamp(pitch, deg_to_rad(-60.0), deg_to_rad(-30.0))
+		yaw = wrap(yaw, 0.0, deg_to_rad(360))
+		camera_pivot.global_rotation = Vector3(deg_to_rad(-35), yaw, 0.0)
+		
+func is_grounded() -> bool:
+	return grounded || is_on_floor()
