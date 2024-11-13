cec229c866194a795c2685484346a6872bc36634
Bazouz660 <basile.trebus-hamann@epitech.eu>
2024-11-13 19:44:45 +0100

    [ADD]: update workflow

--- manager/material_sampler.gd
+++ manager/material_sampler.gd
@@ -14,7 +14,6 @@ class_name MaterialDetector
 # Internal variables
 var _target: Node3D
 var _ray_cast: RayCast3D
-var _check_timer: float = 0.0
 var _current_material: Material = null
 var _last_position: Vector3 = Vector3.ZERO
 
@@ -28,7 +27,7 @@ func _ready() -> void:
 	_ray_cast = RayCast3D.new()
 	_ray_cast.target_position = Vector3(0, -detection_distance, 0)
 	add_child(_ray_cast)
-	
+
 	# Get target node
 	if not target_path.is_empty():
 		_target = get_node(target_path)
@@ -37,48 +36,36 @@ func _ready() -> void:
 	else:
 		# If no target specified, use parent
 		_target = get_parent() if get_parent() is Node3D else null
-		
+
 	if not _target:
 		push_error("MaterialDetector: No valid target found. Ensure the node is attached to or targeting a Node3D.")
-
-#func _physics_process(delta: float) -> void:
-	#if not _target:
-		#return
-		#
-	#if not check_every_frame:
-		#_check_timer += delta
-		#if _check_timer < check_interval:
-			#return
-		#_check_timer = 0.0
-	#
-	#check_material()
 
 func check_material() -> Dictionary:
 	# Update raycast position to target's position
 	_ray_cast.global_position = _target.global_position
-	
+
 	if not _ray_cast.is_colliding():
 		if _current_material != null:
 			_current_material = null
 			emit_signal("material_changed", null)
 		return {}
-	
+
 	var collision_point = _ray_cast.get_collision_point()
-	
+
 	# Skip if we haven't moved enough
 	if collision_point.distance_to(_last_position) < 0.01:
 		return {}
-		
+
 	_last_position = collision_point
-	
+
 	var collider = _ray_cast.get_collider()
 	if not collider:
 		return {}
-		
+
 	var mesh_instance = find_mesh_instance(collider)
 	if not mesh_instance:
 		return {}
-		
+
 	var material = get_material_from_mesh(mesh_instance)
 	var string : String
 	var type: String
@@ -90,8 +77,8 @@ func check_material() -> Dictionary:
 			type = TerrainSystem.get_biome_material()
 	else:
 		type = "Unknown"
-		#print("Material type: Unknown")	
-		
+		#print("Material type: Unknown")
+
 	var surface_info = {
 		"material": material,
 		"mesh_instance": mesh_instance,
@@ -107,41 +94,41 @@ func check_material() -> Dictionary:
 		emit_signal("material_changed", material)
 		# Emit detailed surface information
 		emit_signal("surface_detected", surface_info)
-		
+
 	return surface_info
 
 func find_mesh_instance(node: Node) -> MeshInstance3D:
 	# First check if the node itself is a MeshInstance3D
 	if node is MeshInstance3D:
 		return node
-		
+
 	# Check if it has a direct child that's a MeshInstance3D
 	for child in node.get_children():
 		if child is MeshInstance3D:
 			return child
-			
+
 	# Search parent and siblings
 	var parent = node.get_parent()
 	if parent:
 		for sibling in parent.get_children():
 			if sibling is MeshInstance3D:
 				return sibling
-				
+
 	return null
-	
+
 func get_material_from_mesh(mesh_instance: MeshInstance3D) -> Material:
 	# Try to get material from mesh instance surface
 	var material = mesh_instance.get_active_material(0)
 	if material:
 		return material
-		
+
 	# If no override material, try to get it from the mesh itself
 	var mesh = mesh_instance.mesh
 	if mesh:
 		material = mesh.surface_get_material(0)
 		if material:
 			return material
-			
+
 	return null
 
 ## Returns the current material being detected
